c6d2edf3050407a0ff7399389ebfcf535c08e970
Evan Felix <karcaw@gmail.com>
2020-03-03 16:33:37 -0800

    Cleanup code formatting, and remove macros

--- hello.c
+++ hello.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
-#define A printf
-#define B )
-#define C (
-#define D ;
-#define E(x) #x
-#define F(h) void main() {h}
-
-F(A C E(Hello World) B D)
+void main() {
+	printf ( "Hello World" );
+}
 
